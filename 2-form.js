810e1313b2bd52af8541a74cf586c7a1153efc74
Anastasiia-tyk <anastasiia0matsiuk@gmail.com>
2026-01-29 23:31:31 +0000

    Deploying to gh-pages from @ Anastasiia-tyk/goit-js-hw-09@16f9cd9d5d8f5108f168e7e61657bce6032da21b 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector(".feedback-form");t.addEventListener("submit",e=>(e.preventDefault(),alert("Fill please all fields")));
+//# sourceMappingURL=2-form.js.map
